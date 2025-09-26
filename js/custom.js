836cf6c9d1c847bd907f26354bb871f71ff5d117
asimzada <87058553+asimzada@users.noreply.github.com>
2025-09-26 06:32:33 +0300

    Updated website files

--- js/custom.js
+++ js/custom.js
@@ -3,27 +3,21 @@
   
   "use strict";
 
-    // COUNTER NUMBERS
-    jQuery('.counter-thumb').appear(function() {
-      jQuery('.counter-number').countTo();
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
     });
-
-    // BACKSTRETCH SLIDESHOW
-    $('.hero-section').backstretch([
-      "images/slideshow/afro-woman-cleaning-window-with-rag-home.jpg", 
-      "images/slideshow/afro-woman-holding-bucket-with-cleaning-items.jpg",
-      "images/slideshow/unrecognizable-cleaner-walking-into-hotel-room-with-tools-detergents.jpg"
-    ],  {duration: 2000, fade: 750});
     
     // CUSTOM LINK
     $('.smoothscroll').click(function(){
       var el = $(this).attr('href');
       var elWrapped = $(el);
+      var header_height = $('.navbar').height();
   
-      scrollToDiv(elWrapped);
+      scrollToDiv(elWrapped,header_height);
       return false;
   
-      function scrollToDiv(element){
+      function scrollToDiv(element,navheight){
         var offset = element.offset();
         var offsetTop = offset.top;
         var totalScroll = offsetTop-navheight;
@@ -33,7 +27,27 @@
         }, 300);
       }
     });
-    
+
+    $(window).on('scroll', function(){
+      function isScrollIntoView(elem, index) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(window).height()*.5;
+        if(elemBottom <= docViewBottom && elemTop >= docViewTop) {
+          $(elem).addClass('active');
+        }
+        if(!(elemBottom <= docViewBottom)) {
+          $(elem).removeClass('active');
+        }
+        var MainTimelineContainer = $('#vertical-scrollable-timeline')[0];
+        var MainTimelineContainerBottom = MainTimelineContainer.getBoundingClientRect().bottom - $(window).height()*.5;
+        $(MainTimelineContainer).find('.inner').css('height',MainTimelineContainerBottom+'px');
+      }
+      var timeline = $('#vertical-scrollable-timeline li');
+      Array.from(timeline).forEach(isScrollIntoView);
+    });
+  
   })(window.jQuery);
 
 
